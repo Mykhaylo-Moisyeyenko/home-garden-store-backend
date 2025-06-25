5a32026f8b90004afa912413c8032583eb0e2eee
Mykhaylo Moisyeyenko <moiceenko.mihail@gmail.com>
2025-06-26 01:13:47 +0200

    Инициализация проекта Spring Boot

---
+++ src/test/java/com/homegarden/store/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.homegarden.store.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
