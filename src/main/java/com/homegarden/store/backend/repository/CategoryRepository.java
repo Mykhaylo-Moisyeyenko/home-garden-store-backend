c324e804aacaf59dc2cc3f7380908d1f84776b64
Elizaveta <aquariushausastro@gmail.com>
2025-06-28 19:15:22 +0200

    Добавлен интерфейс CategoryRepository

---
+++ src/main/java/com/homegarden/store/backend/repository/CategoryRepository.java
@@ -0,0 +1,12 @@
+package com.homegarden.store.backend.repository;
+
+import com.homegarden.store.backend.model.entity.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+    Optional<Category> findByName(String name);
+}
