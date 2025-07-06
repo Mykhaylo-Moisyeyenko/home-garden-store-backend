53381784414446046e3b18ec8dfe71ab9c0def58
Mykhaylo Moisyeyenko <moiceenko.mihail@gmail.com>
2025-07-06 19:54:10 +0200

    Создан класс ProductFilterSpecification для фильтрации Product. (#32)
    * Создан класс ProductFilterSpecification для фильтрации Product.
    
    Добавлен пустой класс Product для работоспособности ProductFilterSpecification
    
    * Исправлена ошибка в названии поля discountPrice.
    
    Сборка итогового предиката осуществляется только в конце метода.

---
+++ src/main/java/com/homegarden/store/backend/repository/specification/ProductFilterSpecification.java
@@ -0,0 +1,50 @@
+package com.homegarden.store.backend.repository.specification;
+
+import com.homegarden.store.backend.model.entity.Product;
+import jakarta.persistence.criteria.Path;
+import jakarta.persistence.criteria.Predicate;
+import org.springframework.data.jpa.domain.Specification;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ProductFilterSpecification {
+
+    public static Specification<Product> filter(
+            Long categoryId,
+            Double minPrice,
+            Double maxPrice,
+            Boolean discount,
+            String sort
+    ) {
+        return (root, query, criteriaBuilder) -> {
+            List<Predicate> predicates = new ArrayList<>();
+
+            if (categoryId != null) {
+                predicates.add(criteriaBuilder.equal(root.get("categoryId"), categoryId));
+            }
+
+            if (minPrice != null) {
+                predicates.add(criteriaBuilder.ge(root.get("price"), minPrice));
+            }
+
+            if (maxPrice != null) {
+                predicates.add(criteriaBuilder.le(root.get("price"), maxPrice));
+            }
+
+            if (discount != null && discount == true) {
+                predicates.add(criteriaBuilder.isNotNull(root.get("discountPrice")));
+            }
+
+            if (sort != null) {
+                Path<?> sortPath = root.get("price");
+                if (sort.equalsIgnoreCase("ASC")) {
+                    query.orderBy(criteriaBuilder.asc(sortPath));
+                } else if (sort.equalsIgnoreCase("DESC")) {
+                    query.orderBy(criteriaBuilder.desc(sortPath));
+                }
+            }
+            return criteriaBuilder.and(predicates.toArray(new Predicate[0]));
+        };
+    }
+}
