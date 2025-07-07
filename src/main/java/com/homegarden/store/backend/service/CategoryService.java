e568a297749bd14dffa15742433a220fa448bb42
Sofia <koriakina.sofie@gmail.com>
2025-07-07 23:08:53 +0200

    reformat CategoryService

--- src/main/java/com/homegarden/store/backend/service/CategoryService.java
+++ src/main/java/com/homegarden/store/backend/service/CategoryService.java
@@ -1,17 +1,18 @@
 package com.homegarden.store.backend.service;
 
-import com.homegarden.store.backend.model.dto.CategoryDto;
 import com.homegarden.store.backend.model.entity.Category;
 
 import java.util.List;
 
 public interface CategoryService {
 
-    //убрала дто
     Category update(Long categoryId, String name);
+
     Category create(Category category);
+
     List<Category> getAll();
+
     Category getById(Long id);
+
     void delete(Long id);
 }
-
