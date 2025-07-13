9020ad4d1161b190f9e8592ccc527c31e20e5d24
Mykhaylo Moisyeyenko <moiceenko.mihail@gmail.com>
2025-07-13 13:44:28 +0200

    Update CreateCartItemRequestDTO.java

--- src/main/java/com/homegarden/store/backend/model/dto/CreateCartItemRequestDTO.java
+++ src/main/java/com/homegarden/store/backend/model/dto/CreateCartItemRequestDTO.java
@@ -3,5 +3,5 @@ package com.homegarden.store.backend.model.dto;
 public record CreateCartItemRequestDTO(
         Long cartId,
         Long productId,
-        Integer quantity)
-{}
+        Integer quantity){
+}
