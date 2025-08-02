c5bed35b58a181eff5a67f4f444b8b721ed27207
Elizaveta-Erler <aquariushausastro@gmail.com>
2025-08-02 15:30:56 +0200

    Update PaymentConverter.java

--- src/main/java/com/homegarden/store/backend/converter/PaymentConverter.java
+++ src/main/java/com/homegarden/store/backend/converter/PaymentConverter.java
@@ -1,19 +1,18 @@
 package com.homegarden.store.backend.converter;
 
+import com.homegarden.store.backend.dto.PaymentCreateDTO;
 import com.homegarden.store.backend.dto.PaymentResponseDTO;
 import com.homegarden.store.backend.entity.Order;
 import com.homegarden.store.backend.entity.Payment;
 import org.springframework.stereotype.Component;
 
-import java.math.BigDecimal;
-
 @Component
 public class PaymentConverter {
 
-    public Payment toEntity(Long orderId, BigDecimal amount) {
+    public Payment toEntity(PaymentCreateDTO dto) {
         return Payment.builder()
-                .order(Order.builder().orderId(orderId).build())
-                .amount(amount)
+                .order(Order.builder().orderId(dto.orderId()).build())
+                .amount(dto.amount())
                 .build();
     }
 
