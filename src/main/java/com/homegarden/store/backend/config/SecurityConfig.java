a8b14893bddab0494d6c9a3f10bb6653ffdeea42
Mykhaylo Moisyeyenko <moiceenko.mihail@gmail.com>
2025-08-08 20:08:27 +0200

    Исправлен доступ пользователей к ендпойнтам.

--- src/main/java/com/homegarden/store/backend/config/SecurityConfig.java
+++ src/main/java/com/homegarden/store/backend/config/SecurityConfig.java
@@ -17,7 +17,7 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 
 @Configuration
 @EnableWebSecurity
-@EnableMethodSecurity(prePostEnabled = true)
+@EnableMethodSecurity
 public class SecurityConfig {
 
     @Bean
