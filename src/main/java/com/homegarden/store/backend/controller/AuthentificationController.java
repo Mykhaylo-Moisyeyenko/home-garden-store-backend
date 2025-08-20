7e32e7617c6fd6bad3f41e45287c0b246beed104
Elizaveta-Erler <aquariushausastro@gmail.com>
2025-08-20 22:16:36 +0200

    Update AuthentificationController.java

--- src/main/java/com/homegarden/store/backend/controller/AuthentificationController.java
+++ src/main/java/com/homegarden/store/backend/controller/AuthentificationController.java
@@ -5,6 +5,7 @@ import com.homegarden.store.backend.dto.security.LoginRequestDto;
 import com.homegarden.store.backend.dto.security.LoginResponseDto;
 import com.homegarden.store.backend.service.security.AuthenticationService;
 import jakarta.validation.Valid;
+import org.springframework.validation.annotation.Validated;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Validated
 @RequiredArgsConstructor
 @RequestMapping("/v1/users/login")
 public class AuthentificationController implements AuthentificationControllerApi {
