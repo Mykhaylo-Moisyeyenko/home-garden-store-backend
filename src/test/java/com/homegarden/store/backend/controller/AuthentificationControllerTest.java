cdab118cc887e533e6bf6a5ea3204c9c06f53eb0
Mykhaylo Moisyeyenko <moiceenko.mihail@gmail.com>
2025-08-20 09:27:03 +0200

    Исправлен тест AuthentificationControllerTest

--- src/test/java/com/homegarden/store/backend/controller/AuthentificationControllerTest.java
+++ src/test/java/com/homegarden/store/backend/controller/AuthentificationControllerTest.java
@@ -1,14 +1,23 @@
 package com.homegarden.store.backend.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.homegarden.store.backend.service.UserService;
+import com.homegarden.store.backend.dto.security.LoginRequestDto;
+import com.homegarden.store.backend.dto.security.LoginResponseDto;
+import com.homegarden.store.backend.service.security.AuthenticationService;
 import com.homegarden.store.backend.service.security.JwtFilter;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
+
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(AuthentificationController.class)
 @AutoConfigureMockMvc(addFilters = false)
@@ -18,59 +27,27 @@ public class AuthentificationControllerTest {
     private MockMvc mockMvc;
 
     @MockitoBean
-    private UserService userService;
-
-    @MockitoBean
-    private PasswordEncoder passwordEncoder;
+    private AuthenticationService authenticationService;
 
     @MockitoBean
     private JwtFilter jwtFilter;
 
     private final ObjectMapper objectMapper = new ObjectMapper();
 
-    private final String loginUrl = "/login";
+    private LoginRequestDto login = new LoginRequestDto("test@email.com",
+            "password_111");
 
-//    @Test
-//    void login_withCorrectPassword_returnsJwtToken() throws Exception {
-//        String email = "user@example.com";
-//        String rawPassword = "validPass";
-//        String encodedPassword = "$2a$10$..."; // фиктивный hash
-//
-//        User mockUser = new User();
-//        mockUser.setEmail(email);
-//        mockUser.setPasswordHash(encodedPassword);
-//
-//        when(userService.getByEmail(email)).thenReturn(mockUser);
-//        when(passwordEncoder.matches(rawPassword, encodedPassword)).thenReturn(true);
-//
-//        LoginRequestDto request = new LoginRequestDto(email, rawPassword);
-//
-//        mockMvc.perform(post(loginUrl)
-//                        .contentType(MediaType.APPLICATION_JSON)
-//                        .content(objectMapper.writeValueAsString(request)))
-//                .andExpect(status().isOk())
-//                .andExpect(jsonPath("$.token").value("здесь будет передаваться jwt-token"));
-//    }
-//
-//    @Test
-//    void login_withIncorrectPassword_returnsUnauthorized() throws Exception {
-//        String email = "user@example.com";
-//        String rawPassword = "invalidPass";
-//        String encodedPassword = "$2a$10$...";
-//
-//        User mockUser = new User();
-//        mockUser.setEmail(email);
-//        mockUser.setPasswordHash(encodedPassword);
-//
-//        when(userService.getByEmail(email)).thenReturn(mockUser);
-//        when(passwordEncoder.matches(rawPassword, encodedPassword)).thenReturn(false);
-//
-//        LoginRequestDto request = new LoginRequestDto(email, rawPassword);
-//
-//        mockMvc.perform(post(loginUrl)
-//                        .contentType(MediaType.APPLICATION_JSON)
-//                        .content(objectMapper.writeValueAsString(request)))
-//                .andExpect(status().isUnauthorized())
-//                .andExpect(jsonPath("$.token").value("Password incorrect"));
-//    }
+    private LoginResponseDto response = new LoginResponseDto("dfiajd;flkasdfjahsdfouahfg");
+
+    @Test
+    void loginTest() throws Exception {
+        when(authenticationService.authenticate(login)).thenReturn(response);
+
+        mockMvc.perform(post("/v1/users/login")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(login)))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("token").value("dfiajd;flkasdfjahsdfouahfg"));
+    }
 }
